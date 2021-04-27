8fcceb9cc7453a3a1b4f730c008fff5b8e5f56d3
webInRoad <1040233545@qq.com>
2021-04-27 08:42:30 +0800

    面向对象

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import js from './07-观察者模式-1.js'
-// alert(js.a)
+import './00-oo-2'
+// class Person {
+// 	constructor(name, age) {
+// 		this.name = name
+// 		this.age = age
+// 	}
+// 	speack() {
+// 		alert(`my name is ${this.name},age is ${this.age}`)
+// 	}
+// }
+// let zhangsan = new Person('zhangsan', 12)
+// zhangsan.speack()
